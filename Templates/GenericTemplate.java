6d33fc86867e2e86bf65f2fe741ee3565e0b34f7
Bill Jojo <jojowil@gmail.com>
2024-12-13 16:15:53 -0500

    first template.

---
+++ Templates/GenericTemplate.java
@@ -0,0 +1,21 @@
+/**
+ * Uncomment the two lines for Scanner support.
+ * The two println() calls are not required, but serve as a reminder
+ * that execution begins in the main() methiod.
+ */
+
+//import java.util.Scanner;
+
+public class ProgramTemplate {
+
+    //static Scanner kb = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("My program has begun!");
+
+        // Java statements go here!
+
+        System.out.println("My program is ending!");
+    }
+
+}
