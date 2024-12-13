62618dfb0746959603b5c37c8f2cd7acd261797c
Bill Jojo <jojowil@gmail.com>
2024-12-13 16:15:38 -0500

    finish chapter 1 code

---
+++ Chapter Source Code/Chapter 1 – The Java Environment/ProgramTemplate.java
@@ -0,0 +1,15 @@
+//import java.util.Scanner;
+
+public class ProgramTemplate {
+
+    //static Scanner kb = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("My program has begun!");
+
+        // Java statements go here!
+
+        System.out.println("My program is ending!");
+    }
+
+}
