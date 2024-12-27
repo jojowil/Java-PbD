2734476ef2ed1c60add4a277feb6597831d74cbd
Bill Jojo <jojowil@gmail.com>
2024-12-27 12:17:27 -0500

    some more projects.

---
+++ Project Solutions/CISS-110/Project 1/Project1_lastname.java
@@ -0,0 +1,25 @@
+public class Project1_lastname {
+
+    public static viod main (string[] args) {
+
+        int x, y;
+        char ch;
+        String s;
+        boolean b = true;
+
+        c = 'K';
+        x = 15;
+        Y = 20;
+
+        System.out.println("Value of x is " + x + ".");
+        System.out.println("Value of y is " + Y + ".");
+        System.out.println("Value of ch is " + c + ".");
+
+        S = "This is a string value assigned to s";
+
+        system.out.println("Value of s is\n*" + s +"*");
+
+        System.out.println("Value of b is " + b);
+
+    } // end of main method
+} // end of class.
