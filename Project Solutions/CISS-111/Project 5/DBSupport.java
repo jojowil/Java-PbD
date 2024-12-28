9ccad16904b2552ae384fce34a6b993a1df1b10a
Bill Jojo <jojowil@gmail.com>
2024-12-28 13:26:02 -0500

    ciss-111 project 5. This time I'm putting the code in!

---
+++ Project Solutions/CISS-111/Project 5/DBSupport.java
@@ -0,0 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBSupport {
+
+    /**
+     * Gets a database connection
+     * @return a Connection object or null on error
+     */
+    public static Connection getDBConn() {
+        Connection mdb;
+        String userName="ciss111p5", password="ciss111p5";
+
+        String url = "jdbc:mysql://crammit.in:7070/ciss111p5";
+        try {
+            mdb = DriverManager.getConnection(url +
+                    "?user="+ userName + "&password=" + password);
+            return mdb;
+        } catch (SQLException ex) {
+            // handle any errors
+            System.err.println("SQLException: " + ex.getMessage());
+            System.err.println("SQLState: " + ex.getSQLState());
+            System.err.println("VendorError: " + ex.getErrorCode());
+            return null;
+        }
+    }
+
+    /**
+     * Gets the open status of a Connection object.
+     * @param mdb the MySQL database object
+     * @return true if connection is open
+     */
+    public static boolean getMysqlStatus(Connection mdb) {
+        if ( mdb != null )
+            try {
+                return !mdb.isClosed();
+            } catch (SQLException e) {
+                return false;
+            }
+        return false;
+    }
+
+    /**
+     * Closes a database connection.
+     * @param mdb the Connection objet to be closed
+     */
+    public static void closeDB(Connection mdb) {
+        try {
+            if ( mdb != null && !mdb.isClosed() )
+                mdb.close();
+        } catch (SQLException e) {
+            System.err.println("Failed closing MySQL connection.");
+        }
+    }
+}
--- Project Solutions/CISS-111/Project 5/OrderedList.java
+++ Project Solutions/CISS-111/Project 5/OrderedList.java
@@ -1,35 +1,35 @@
 import java.util.Iterator;
 
-public class OrderedList implements Iterable<Integer> {
+public class OrderedList implements Iterable<String> {
     private Node head;
 
     // This is the requirement of implementing Iterable. Provide
     // an iterator() method that returns an Iterator object.
-    public Iterator<Integer> iterator() {
+    public Iterator<String> iterator() {
         return new LLIterator();
     }
 
     // This is what the iterator() returns to the caller.
     // This is the iterator object that processes the list.
-    private class LLIterator implements Iterator<Integer> {
+    private class LLIterator implements Iterator<String> {
         private Node current = head;
 
         public boolean hasNext() {
             return current != null;
         }
 
-        public Integer next() {
-            int item = current.data;
+        public String next() {
+            String item = current.data;
             current = current.next;
             return item;
         }
     }
 
     private class Node {
-        private int data;
+        private String data;
         private Node next;
 
-        private Node(int item) {
+        private Node(String item) {
             data = item;
             next = null;
         }
@@ -39,19 +39,29 @@ public class OrderedList implements Iterable<Integer> {
         head = null;
     }
 
-    public void insert(int item) {
+    public void insert(String item) {
         Node cur = head, back = null;
 
         Node n = new Node(item);
-        n.next = head;
-        head = n;
+        while (cur != null) {
+            if (cur.data.equals(item)) return; // no dups!
+
+            if (cur.data.compareToIgnoreCase(item) > 0) break;
+            back = cur;
+            cur = cur.next;
+        }
+        n.next = cur;
+        if (back == null)
+            head = n;
+        else
+            back.next = n;
     }
 
-    public void delete(int item) {
+    public void delete(String item) {
         Node cur = head, back = null;
 
         while (cur != null) {
-            if (cur.data == item) {
+            if (cur.data.equals(item)) {
                 if (back == null)
                     head = cur.next;
                 else
@@ -80,4 +90,3 @@ public class OrderedList implements Iterable<Integer> {
         return o + "]";
     }
 }
-
