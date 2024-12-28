5e27e964a92638216757c168901497806b3dc3b4
Bill Jojo <jojowil@gmail.com>
2024-12-28 13:16:14 -0500

    ciss-111 project 5. might need tidying, but good enough for now.

---
+++ Project Solutions/CISS-111/Project 5/OrderedList.java
@@ -0,0 +1,83 @@
+import java.util.Iterator;
+
+public class OrderedList implements Iterable<Integer> {
+    private Node head;
+
+    // This is the requirement of implementing Iterable. Provide
+    // an iterator() method that returns an Iterator object.
+    public Iterator<Integer> iterator() {
+        return new LLIterator();
+    }
+
+    // This is what the iterator() returns to the caller.
+    // This is the iterator object that processes the list.
+    private class LLIterator implements Iterator<Integer> {
+        private Node current = head;
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Integer next() {
+            int item = current.data;
+            current = current.next;
+            return item;
+        }
+    }
+
+    private class Node {
+        private int data;
+        private Node next;
+
+        private Node(int item) {
+            data = item;
+            next = null;
+        }
+    }
+
+    public OrderedList () {
+        head = null;
+    }
+
+    public void insert(int item) {
+        Node cur = head, back = null;
+
+        Node n = new Node(item);
+        n.next = head;
+        head = n;
+    }
+
+    public void delete(int item) {
+        Node cur = head, back = null;
+
+        while (cur != null) {
+            if (cur.data == item) {
+                if (back == null)
+                    head = cur.next;
+                else
+                    back.next = cur.next;
+
+                break; //leave the loop
+            } else {
+                back = cur;
+                cur = cur.next; // move to the next node
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String o = "[";
+        Iterator iter = iterator();
+
+        // first data value is alone.
+        if ( iter.hasNext() )
+            o += iter.next();
+        // remaining data values have ", value"
+        while ( iter.hasNext() )
+            o += ", " + iter.next();
+
+        return o + "]";
+    }
+}
+
