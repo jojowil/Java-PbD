01677c0727d5f8563e3a3b6b2fa6761d5349bca7
jojowil <jojowil@gmail.com>
2026-01-09 11:30:38 -0500

    code reformat.

--- Chapter Source Code/Chapter 1 – The Java Environment/FirstJavaProgram.java
+++ Chapter Source Code/Chapter 1 – The Java Environment/FirstJavaProgram.java
@@ -1,9 +1,5 @@
 public class FirstJavaProgram {
-
     public static void main(String[] args) {
-
         System.out.println("Woohoo! My first program!");
-
     }
-
 }
--- Chapter Source Code/Chapter 1 – The Java Environment/ProgramTemplate.java
+++ Chapter Source Code/Chapter 1 – The Java Environment/ProgramTemplate.java
@@ -1,13 +1,12 @@
 /**
  * Uncomment the two lines for Scanner support.
  * The two println() calls are not required but serve as a reminder
- * that execution begins in the main() methiod.
+ * that execution begins in the main() method.
  */
 
 //import java.util.Scanner;
 
 public class ProgramTemplate {
-
     //static Scanner kb = new Scanner(System.in);
 
     public static void main(String[] args) {
