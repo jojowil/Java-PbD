1f585659a8cbcccc07dc6e60f37fec5ac63a2c0d
jojowil <jojowil@gmail.com>
2026-01-09 13:27:01 -0500

    refactoring

--- Chapter Source Code/Chapter 12 – Exceptions/DataFormatException.java
+++
@@ -1,9 +0,0 @@
-public class DataFormatException extends Exception {
-    public DataFormatException () {
-        super("Data must contain at least one colon");
-    }
-
-    public DataFormatException(String msg) {
-        super(msg);
-    }
-}
